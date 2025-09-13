f19c851e690c6e8e20258b41fe267fa0c349e1c4
pixelandbrick <ryan@pixel-brick.com>
2025-09-13 00:25:59 -0400

    first commit

---
+++ modules/ps_newproducts/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'acad8d2f785ffb60143fc74d2be2b8ab7ae04ecc',
+        'name' => 'prestashop/ps_newproducts',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/ps_newproducts' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'acad8d2f785ffb60143fc74d2be2b8ab7ae04ecc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
